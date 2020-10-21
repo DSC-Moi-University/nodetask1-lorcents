64293075e7fdbbc2c78094be378336ff4a554ea6
lorcents <iannjoroge120@outlook.com>
2020-10-21 21:37:33 +0300

    commit1.1

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 function socksForSale() {
     //write your code here
-    //dummy answer
-    var a = 3;
-    return a;
+    const socks = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+    socks.sort(function(a, b) {
+        return a - b;
+    });
+    var counter = 0;
+    for (var i = 0; i < socks.length - 1; i++) {
+        if (socks[i] === socks[i + 1]) {
+            counter++;
+            i++;
+        }
+    }
+    return counter;
 }
 
 
